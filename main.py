fcef3ab00b9b639390af894b928b01ab2b2b3e73
botalov <89090168690@mail.ru>
2024-02-28 15:45:40 +0500

    add file

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def main_app(dt_from, dt_upto, group_type):
 
 
 
-#dt_from = dt.datetime.strptime("2022-02-01T00:00:00", "%Y-%m-%dT%H:%M:%S")
-#dt_upto = dt.datetime.strptime("2022-02-02T00:00:00", "%Y-%m-%dT%H:%M:%S")
-#group_type = "hour"
-#print(main_app(dt_from, dt_upto, gro))
+# dt_from = dt.datetime.strptime("2022-02-01T00:00:00", "%Y-%m-%dT%H:%M:%S")
+# dt_upto = dt.datetime.strptime("2022-02-02T00:00:00", "%Y-%m-%dT%H:%M:%S")
+# group_type = "hour"
+# print(main_app(dt_from, dt_upto, "month"))
