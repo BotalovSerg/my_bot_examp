694f01af2001cdd83e02d97a6f6bb69c25df0b79
Botalov Serg <89090168690@mail.ru>
2024-03-01 18:36:47 +0500

    add task

--- tasks/task_4.py
+++ tasks/task_4.py
@@ -1,17 +1,18 @@
+# N, M, x, y = int(input()),int(input()),int(input()),int(input())
+
+# M_y = M - y
+# N_x = N - x
+
+# print(min(x, y, M_y, N_x))
 
 def main():
-    n = int(input())
-    s = 0
-    lst = [[0] * 3 for i in range(20)]
-    lst[0][0]=lst[0][1]=lst[0][2]=1
-    for i in range(1, n):
-        lst[i][0]=lst[i-1][1]+lst[i-1][2]
-        lst[i][2]=lst[i][1]=lst[i-1][0]+lst[i-1][1]+lst[i-1][2]
-    for i in range(3):
-        s += lst[n-1][i]
-    
-    print(s)
+    N, M, x, y = int(input()),int(input()),int(input()),int(input())
+    N, M = min(N, M), max(N, M)
+    if x == 0 or y == 0:
+        return 0
+    M_y = M - y
+    N_x = N - x
+    return min(x, y, M_y, N_x)
 
-if __name__=='__main__':
-    main()
-
+if __name__ == '__main__':
+    print(main())
