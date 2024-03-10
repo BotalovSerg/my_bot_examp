28ae8ce9abd392c6b28cd108792a4b8b748a83c4
Botalov Serg <89090168690@mail.ru>
2024-03-10 22:08:19 +0500

    add task 15

--- tasks/task_15.py
+++ tasks/task_15.py
@@ -1,9 +1,16 @@
 # O. Файлы-файлы
-import re
+# import re
 # s =  input()
 
-st = "abacaba123.hlm и 123.brhl интересуют его, а a.brhl.py , также hlm.cpp .brhl нет."
+# st = "Например, файлы abacaba123.hlm и 123.brhl интересуют его, а aAacaba123.hlm a.brhl.py и hlm.cpp нет. Также Пахома не интересуют имена, .hlm, .brhl"
+# st1 = "12:21:10 12/01/2022 127.0.0.1 error abc.hlm is broken."
+res = []
 
-pattern = "\w+\.(?:hlm|brhl)[^\.]"
-res = re.findall(pattern, st)
-print(res)
+with open("input_15.txt") as f:
+    s = f.read()
+
+
+for w in s.split():
+    if w.endswith('.hlm') or w.endswith('.brhl') or w.endswith('.hlm.') or w.endswith('.brhl.'):
+        if w.islower() and not w.startswith('.'):
+            print(w.rstrip('.'))
