ab93f10a2a8f0230cc2efdae265d245bf0eb000f
botalov <89090168690@mail.ru>
2024-03-26 13:44:11 +0500

    update

--- project_tk/notebook/app.py
+++ project_tk/notebook/app.py
@@ -3,14 +3,18 @@ from text_editor import TextEditor
 from main_menu import MainMenu
 
 
-root = Tk()
-root.title('Notebook')
-root.geometry('600x700')
 
-text = TextEditor(root)
-MainMenu(root, text)
 
-if __name__ == '__main__':
+def main():
+    root = Tk()
+    root.title('Notebook')
+    root.geometry('600x700')
+
+    text = TextEditor(root)
+    MainMenu(root, text)
     root.mainloop()
 
+
+if __name__ == '__main__':
+    main()
 
