e4a351f36a8555b137158646f775919beebda3ed
botalov <89090168690@mail.ru>
2024-04-09 13:15:08 +0500

    codewars katas

--- practica/test_codewars.py
+++ practica/test_codewars.py
@@ -6,15 +6,24 @@ def multi(num):
     return res
 
 
-def persistence(n):
-    cnt = 0
-    if 0 < n < 10:
-        return cnt
-    while n >= 10:
-        cnt += 1
-        n = multi(n)
-    return cnt
-print(persistence(4))
-print(persistence(39))
-print(persistence(25))
-print(persistence(999))
+def dig_pow(n, p):
+    res = n
+    n = int(str(n)[::-1])
+    tmp = 0
+    while n > 0:
+        tmp += (n%10)**p
+        n //= 10
+        p += 1
+    if tmp % res == 0:
+        return tmp // res
+    return -1
+
+
+def find_next_square(sq):
+    sqr_num = sq ** 0.5
+    
+    return (sqr_num+1)**2 if sq%sqr_num == 0 else -1
+
+print(find_next_square(121))
+print(find_next_square(625))
+print(find_next_square(114))
