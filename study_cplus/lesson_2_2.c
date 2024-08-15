4a68d0080d15e7cdf24d9d08aa72ecd787ee92e1
botalov <89090168690@mail.ru>
2024-08-15 11:44:30 +0500

    update cpp

---
+++ study_cplus/lesson_2_2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int var, size, hex, oct;
+    int total = 1024;
+    var = size = 100;
+    hex = 0x1FA;
+    oct = 0123;
+
+    char ch;
+
+    ch = 's';
+    printf("char = %c, code = %d\n", ch, ch);
+
+    double d1, d2, d3, d4;
+    d1 = 10.0;
+    d2 = -8.;
+    d3 = 1e2;
+    d4 = 5e-3;
+
+    float var;
+    var = 10.3f;
+
+    int size_float = sizeof(float);
+    int size_char = sizeof(ch);
+
+    return 0;
+}
