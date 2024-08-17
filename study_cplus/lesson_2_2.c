44b0553210ef5a7646c424d7b4723bd05e456811
Botalov Serg <89090168690@mail.ru>
2024-08-17 12:24:19 +0500

    update

--- study_cplus/lesson_2_2.c
+++ study_cplus/lesson_2_2.c
@@ -13,17 +13,21 @@ int main(void)
     ch = 's';
     printf("char = %c, code = %d\n", ch, ch);
 
-    double d1, d2, d3, d4;
+    double d1, d2, d3, d4, d5;
     d1 = 10.0;
     d2 = -8.;
-    d3 = 1e2;
+    d3 = -1e2;
     d4 = 5e-3;
+    d5 = 123;
 
-    float var;
-    var = 10.3f;
+    float var_float;
+    var_float = 10.3f;
 
     int size_float = sizeof(float);
     int size_char = sizeof(ch);
+    int size_d1 = sizeof d1;
+
+    printf("size_d1: %d\n", size_d1);
 
     return 0;
 }
