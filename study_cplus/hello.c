a39d393ea9878ddae315590ad3743f5d1b913b9f
botalov <89090168690@mail.ru>
2024-09-10 08:31:51 +0500

    up

--- study_cplus/hello.c
+++ study_cplus/hello.c
@@ -1,27 +1,11 @@
 #include <stdio.h>
 
+#define TWO     2
+#define FOUR    TWO * 2
+
 int main(void)
 {
-    int start_s = 37985;
-    int in_time, b, d;
-
-    ++(b);
-    d++;
-    --b;
-    // (b++)++;
-    // (d + 10)++;
-    // --(b + d);
-    // 10++;
-
-    int h, m, s;
-    scanf("%d; %d; %d", &h, &m, &s);
-    in_time = h * 3600 + m * 60 + s;
-    start_s += in_time;
-
-    h = start_s / 3600;
-    m = start_s % 3600 / 60;
-    s = start_s % 60;
-    printf("%02d:%02d:%02d\n", h, m, s);
-
+    int b = FOUR;
+    printf("TWO = %d\n", b / 2);
     return 0;
 }
