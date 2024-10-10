fb9d174bc2e021a98671ecabb28626d0f592f7d0
Botalov Sergey <89090168690@mail.ru>
2024-10-10 12:38:19 +0500

    up

--- study_cplus/hello.c
+++ study_cplus/hello.c
@@ -1,28 +1,54 @@
 #include <stdio.h>
 
+void strip_string(char *str, int max_len)
+{
+    int count = 0;
+    while (*str++ != '\0' && count++ < max_len)
+        ;
+
+    if (count > 1)
+    {
+        str -= 2;
+        if (*str == '\n')
+            *str = '\0';
+    }
+}
+
 int main(void)
 {
-    char str[50] = "Best   language  \"C\"";
-    for (int i = 0; str[i] != '\0'; ++i)
+    char str[100];
+    int indx = 0, count = 0, start = 0, end = 0;
+    fgets(str, sizeof(str), stdin);
+    strip_string(str, sizeof(str));
+
+    while (str[indx] != '\0')
     {
-        if (str[i] == ' ')
+        if (str[indx] != ' ')
         {
-            if (str[i - 1] != '\n')
+            int i = indx;
+            start = indx;
+            while (str[i] != ' ' && str[i + 1] != '\0')
             {
-                str[i] = '\n';
+                ++i;
             }
-            else
+            indx = i;
+            end = i;
+            count += 1;
+        }
+        ++indx;
+        if (count == 2)
+        {
+            if (end == start)
+                printf("%c", start[start]);
+            for (; start < end; ++start)
             {
-                int tmp = i;
-                while (str[tmp] == ' ')
-                {
-                    for (int j = tmp; str[j] != '\0'; ++j)
-                    {
-                        str[j] = str[j + 1];
-                    }
-                }
+                printf("%c", str[start]);
             }
+            return 0;
         }
     }
+
+    puts("no");
+
     return 0;
 }
