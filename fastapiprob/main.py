280877a33b5b6874c5fb3007273c9a16510f7eb2
Botalov Sergey <89090168690@mail.ru>
2024-10-24 14:49:50 +0500

    repeat fastapi

---
+++ fastapiprob/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import uvicorn
+
+from items_views import router as items_router
+from users.views import router as users_router
+
+app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
+
+
+@app.get("/")
+def hello_index():
+    return {"message": "Hello index!"}
+
+
+@app.get("/hello/")
+def hello(name: str = "User"):
+    return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
