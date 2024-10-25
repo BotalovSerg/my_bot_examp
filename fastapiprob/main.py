e84022b7210d92d30b24e92dbfb0f0ba1fefae68
Botalov Sergey <89090168690@mail.ru>
2024-10-25 11:18:25 +0500

    added table

--- fastapiprob/main.py
+++ fastapiprob/main.py
@@ -11,8 +11,8 @@ from users.views import router as users_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    # async with db_helper.engine.begin() as conn:
+    #     await conn.run_sync(Base.metadata.create_all)
 
     yield
 
