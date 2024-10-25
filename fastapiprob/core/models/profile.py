b64f8a14cc2e2773b318e5937f3d940b7611eceb
Botalov Sergey <89090168690@mail.ru>
2024-10-25 11:50:12 +0500

    added mixins

---
+++ fastapiprob/core/models/profile.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from .base import Base
+from .mixins import UserRelationMixin
+
+
+class Profile(UserRelationMixin, Base):
+    _user_id_unique = True
+    _user_back_populates = "profile"
+
+    first_name: Mapped[str | None] = mapped_column(String(50))
+    last_name: Mapped[str | None] = mapped_column(String(50))
+    bio: Mapped[str | None]
