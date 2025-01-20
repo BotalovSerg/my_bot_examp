8132d2bdf51441af68662920cd60adbc4923266b
Botalov Sergey <89090168690@mail.ru>
2025-01-20 10:14:06 +0500

    up

--- course_cpp/ts_987.cpp
+++ course_cpp/ts_987.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <math.h>
 
+using std::max;
+using std::min;
+
 enum
 {
     corners = 4
@@ -22,14 +25,29 @@ double get_area_triangle(double a, double b, double c)
     return sqrt(p * (p - a) * (p - b) * (p - c));
 }
 
+bool is_point_in_polygon(int x, int y, point figure[], const double total_area)
+{
+    double current_area = 0;
+
+    for (int i = 0; i < corners; i++)
+    {
+        int next = (i + 1) % corners;
+        double a = get_len_segment(figure[i].x, figure[i].y, figure[next].x, figure[next].y);
+        double b = get_len_segment(figure[i].x, figure[i].y, x, y);
+        double c = get_len_segment(figure[next].x, figure[next].y, x, y);
+        current_area += get_area_triangle(a, b, c);
+    }
+
+    return fabs(total_area - current_area) < 1e-10;
+}
+
 int main()
 {
     point figure[corners];
-    point pt;
+    int x_min, x_max, y_min, y_max, result = 0, count = 0;
     const double eps = 1e-10;
-    double s1, s2, s3, s4, s5, area1, area2, s_x1, s_x2, s_x3, s_x4, area3, area4, area5, area6;
+    double s1, s2, s3, s4, s5, total_area;
 
-    int count = 0;
     while (count < corners && scanf("%d; %d", &figure[count].x, &figure[count].y) == 2)
         count++;
 
@@ -39,6 +57,11 @@ int main()
         return 1;
     }
 
+    x_min = min(min(min(figure[0].x, figure[1].x), figure[2].x), figure[3].x);
+    x_max = max(max(max(figure[0].x, figure[1].x), figure[2].x), figure[3].x);
+    y_min = min(min(min(figure[0].y, figure[1].y), figure[2].y), figure[3].y);
+    y_max = max(max(max(figure[0].y, figure[1].y), figure[2].y), figure[3].y);
+
     s1 = get_len_segment(figure[0].x, figure[0].y, figure[1].x, figure[1].y);
     s2 = get_len_segment(figure[1].x, figure[1].y, figure[2].x, figure[2].y);
     s3 = get_len_segment(figure[2].x, figure[2].y, figure[3].x, figure[3].y);
@@ -46,17 +69,19 @@ int main()
 
     s5 = get_len_segment(figure[0].x, figure[0].y, figure[2].x, figure[2].y);
 
-    area1 = get_area_triangle(s1, s2, s5);
-    area2 = get_area_triangle(s3, s4, s5);
+    total_area = get_area_triangle(s1, s2, s5) + get_area_triangle(s3, s4, s5);
 
-    if (fabs((area1 + area2) - (area3 + area4 + area5 + area6)) < eps)
+    for (int i = x_min; i <= x_max; ++i)
     {
-        std::cout << "yes" << '\n';
-    }
-    else
-    {
-        std::cout << "no" << '\n';
+        for (int j = y_min; j <= y_max; ++j)
+        {
+            if (is_point_in_polygon(i, j, figure, total_area))
+            {
+                ++result;
+            }
+        }
     }
 
+    std::cout << result << std::endl;
     return 0;
 }
