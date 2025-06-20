db0fa56aa391d369020c2879778debc67e416691
Botalov Sergey <89090168690@mail.ru>
2025-06-20 10:03:38 +0500

    practica c++

---
+++ course_cpp_oop/ts_3.9.7.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+class CoordsND
+{
+    enum
+    {
+        max_coords = 10
+    };
+    int *coords{nullptr}; // массив значений координат
+    int size{0};          // количество координат (не более max_coords)
+public:
+    CoordsND() {}
+    CoordsND(int *lst, int sz)
+    {
+        size = (sz > max_coords) ? max_coords : sz;
+
+        coords = new int[size];
+        for (int i = 0; i < size; ++i)
+            coords[i] = lst[i];
+    }
+    ~CoordsND() { delete[] coords; }
+
+    int *get_coords() { return coords; }
+    int get_size() const { return size; }
+
+    CoordsND(const CoordsND &other)
+    {
+        delete[] coords;
+        this->size = other.size;
+        this->coords = new int(size);
+        for (int i = 0; i < size; i++)
+        {
+            coords[i] = other.coords[i];
+        }
+    }
+    CoordsND(CoordsND &&move) noexcept : size(move.size)
+    {
+        this->coords = move.coords;
+        move.coords = nullptr;
+    }
+    CoordsND &operator=(CoordsND &right)
+    {
+        if (this == &right)
+        {
+            return *this;
+        }
+        delete[] coords;
+        size = right.size;
+        this->coords = new int(size);
+        for (int i = 0; i < size; i++)
+        {
+            coords[i] = right.coords[i];
+        }
+        return *this;
+    }
+    CoordsND &operator=(CoordsND &&right) noexcept
+    {
+        delete[] coords;
+        size = right.size;
+        this->coords = right.coords;
+        right.coords = nullptr;
+
+        return *this;
+    }
+};
+
+int main(void)
+{
+    int lst[]{-4, 10, 0, 77, 14};
+    CoordsND coords{lst, std::size(lst)};
+
+    return 0;
+}
