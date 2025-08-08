46700223aac4e41b20e1ff05d9f4e31f59d6046c
Botalov Sergey <89090168690@mail.ru>
2025-08-08 11:32:54 +0500

    python task

---
+++ sob/decorator_cache.py
@@ -0,0 +1,61 @@
+import unittest.mock
+
+
+def lru_cache(maxsize: int = None):
+    def decorator_func(func):
+        def wrapper_func(*args, **kwargs):
+            print(f"Вызов функции {func.__name__} с аргументами: {args}, {kwargs}")
+            if maxsize:
+                print("maxsize", maxsize)
+            result = func(*args, **kwargs)
+            print(f"Результат функции: {result}")
+            return result
+
+        return wrapper_func
+
+    return decorator_func
+
+
+@lru_cache
+def sum(a: int, b: int) -> int:
+    return a + b
+
+
+@lru_cache
+def sum_many(a: int, b: int, *, c: int, d: int) -> int:
+    return a + b + c + d
+
+
+@lru_cache(maxsize=3)
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+if __name__ == "__main__":
+
+    sum(1, 2)
+    sum(3, 4)
+    multiply(1, 2)
+    multiply(3, 4)
+    sum_many(1, 2, c=3, d=4)
+
+    # assert sum(1, 2) == 3
+    # assert sum(3, 4) == 7
+
+    # assert multiply(1, 2) == 2
+    # assert multiply(3, 4) == 12
+
+    # assert sum_many(1, 2, c=3, d=4) == 10
+
+    # mocked_func = unittest.mock.Mock()
+    # mocked_func.side_effect = [1, 2, 3, 4]
+
+    # decorated = lru_cache(maxsize=2)(mocked_func)
+    # assert decorated(1, 2) == 1
+    # assert decorated(1, 2) == 1
+    # assert decorated(3, 4) == 2
+    # assert decorated(3, 4) == 2
+    # assert decorated(5, 6) == 3
+    # assert decorated(5, 6) == 3
+    # assert decorated(1, 2) == 4
+    # assert mocked_func.call_count == 4
