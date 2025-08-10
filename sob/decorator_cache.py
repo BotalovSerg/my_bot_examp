cd2736308e9161d4633b65f66d29f35dfa18ff9f
Botalov Sergey <89090168690@mail.ru>
2025-08-10 21:07:02 +0500

    practica python

--- sob/decorator_cache.py
+++ sob/decorator_cache.py
@@ -1,19 +1,31 @@
 import unittest.mock
+import functools
 
 
-def lru_cache(maxsize: int = None):
-    def decorator_func(func):
-        def wrapper_func(*args, **kwargs):
-            print(f"Вызов функции {func.__name__} с аргументами: {args}, {kwargs}")
-            if maxsize:
-                print("maxsize", maxsize)
-            result = func(*args, **kwargs)
-            print(f"Результат функции: {result}")
-            return result
+def lru_cache(*args, **kws):
+    if len(args) == 1 and callable(args[0]) and not kws:
+        func = args[0]
 
-        return wrapper_func
+        @functools.wraps(func)
+        def inner(*func_arg, **func_kws):
+            res = func(*func_arg, **func_kws)
+            return res
 
-    return decorator_func
+        return inner
+    else:
+        maxsize = kws.get("maxsize", 1000)
+
+        def decorator_func(func):
+            @functools.wraps(func)
+            def wrapper_func(*func_arg, **func_kws):
+                if maxsize:
+                    print("maxsize", maxsize)
+                result = func(*func_arg, **func_kws)
+                return result
+
+            return wrapper_func
+
+        return decorator_func
 
 
 @lru_cache
@@ -39,23 +51,23 @@ if __name__ == "__main__":
     multiply(3, 4)
     sum_many(1, 2, c=3, d=4)
 
-    # assert sum(1, 2) == 3
-    # assert sum(3, 4) == 7
+    assert sum(1, 2) == 3
+    assert sum(3, 4) == 7
 
-    # assert multiply(1, 2) == 2
-    # assert multiply(3, 4) == 12
+    assert multiply(1, 2) == 2
+    assert multiply(3, 4) == 12
 
-    # assert sum_many(1, 2, c=3, d=4) == 10
+    assert sum_many(1, 2, c=3, d=4) == 10
 
-    # mocked_func = unittest.mock.Mock()
-    # mocked_func.side_effect = [1, 2, 3, 4]
+    mocked_func = unittest.mock.Mock()
+    mocked_func.side_effect = [1, 2, 3, 4]
 
-    # decorated = lru_cache(maxsize=2)(mocked_func)
-    # assert decorated(1, 2) == 1
-    # assert decorated(1, 2) == 1
-    # assert decorated(3, 4) == 2
-    # assert decorated(3, 4) == 2
-    # assert decorated(5, 6) == 3
-    # assert decorated(5, 6) == 3
-    # assert decorated(1, 2) == 4
-    # assert mocked_func.call_count == 4
+    decorated = lru_cache(maxsize=2)(mocked_func)
+    assert decorated(1, 2) == 1
+    assert decorated(1, 2) == 1
+    assert decorated(3, 4) == 2
+    assert decorated(3, 4) == 2
+    assert decorated(5, 6) == 3
+    assert decorated(5, 6) == 3
+    assert decorated(1, 2) == 4
+    assert mocked_func.call_count == 4
