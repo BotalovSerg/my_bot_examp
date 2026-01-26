8a4b70ce15e4c671c489d44c3e1347d89814c8b8
Sergey Botalov <89090168690@mail.ru>
2026-01-26 20:32:22 +0500

    practica js

---
+++ study_js/step5.1.js
@@ -0,0 +1,25 @@
+const weather = "";
+
+if (weather === "rainy") {
+  console.log("возьмите зонт");
+} else if (weather === "sunny") {
+  console.log("возьмите очки");
+} else if (weather === "cold") {
+  console.log("одевайтесь теплее");
+} else {
+  console.log("хорошего вам дня");
+}
+
+const size = " Маленькая";
+
+if (size === null || size === undefined) {
+  console.log("Вы не выбрали размер!");
+} else if (size.trim().toLocaleLowerCase() === "маленькая") {
+  console.log("Маленькая пицца стоит 800 руб.");
+} else if (size.trim().toLocaleLowerCase() === "средняя") {
+  console.log("Средняя пицца стоит 1200 руб.");
+} else if (size.trim().toLocaleLowerCase() === "большая") {
+  console.log("Большая пицца стоит 1500 руб.");
+} else {
+  console.log("Такого размера у нас нет!");
+}
