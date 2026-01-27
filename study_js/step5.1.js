a51890687f258445013c6543bce839a2898f5d9b
Sergey Botalov <89090168690@mail.ru>
2026-01-27 19:49:09 +0500

    practica js

--- study_js/step5.1.js
+++ study_js/step5.1.js
@@ -23,3 +23,22 @@ if (size === null || size === undefined) {
 } else {
   console.log("Такого размера у нас нет!");
 }
+
+const animal = "";
+
+switch (animal) {
+  case "кошка":
+  case "собака":
+  case "хомяк":
+    console.log("Это домашнее животное.");
+    break;
+  case "рыба":
+  case "птица":
+  case "черепаха":
+  case "ящерица":
+    console.log("Это экзотический питомец.");
+    break;
+  default:
+    console.log("Это мне неизвестно.");
+    break;
+}
