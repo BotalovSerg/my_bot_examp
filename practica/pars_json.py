d17e528f54dc87730d61bf5f04256421301c6efd
Sergey Botalov <89090168690@mail.ru>
2026-02-25 20:24:26 +0500

    practica python

--- practica/pars_json.py
+++ practica/pars_json.py
@@ -9,4 +9,20 @@ def print_sorted_people(json_string: str):
 
 
 people = '[{"name": "Alice Johnson", "country": "USA", "age": 28}, {"name": "Mark Smith", "country": "UK", "age": 35}, {"name": "Sophie Lee", "country": "Australia", "age": 22}, {"name": "Ivan Petrov", "country": "Russia", "age": 30}]'
-print_sorted_people(people)
+# print_sorted_people(people)
+
+
+with open("group_people.json", encoding="utf-8") as f:
+    data = json.load(f)
+    m_count = 0
+    m_id = 0
+    for item in data:
+        count = 0
+        # print(item.get("id_group"))
+        for p in item.get("people"):
+            if p.get("gender") == "Female" and p.get("year") > 1977:
+                count += 1
+        if count > m_count:
+            m_count = count
+            m_id = item.get("id_group")
+    print(f"id {m_id} - count {m_count}")
