d3edeb8dffb095216323510a2b08d34acdefb94a
Sergey Botalov <89090168690@mail.ru>
2026-04-06 21:47:58 +0500

    practica  python

--- practica/save-csv.py
+++ practica/save-csv.py
@@ -22,4 +22,30 @@ def print_column_values(filename: str, column_name: str) -> None:
             print(row[inx_col])
 
 
-print_column_values("library_books.csv", "Автор")
+# print_column_values("library_books.csv", "Автор")
+
+
+def print_columns(filename: str, columns: list[str]) -> None:
+    with open(filename, encoding="utf-8") as f:
+        it = csv.reader(f)
+        header = next(it)
+        list_indexs = []
+        for col in columns:
+            if col in header:
+                list_indexs.append(header.index(col))
+            else:
+                list_indexs.append("N/A")
+
+        if list_indexs.count("N/A") == len(list_indexs):
+            print("Указанные колонки не найдены.")
+            return
+
+        for row in it:
+            for i, val in enumerate(list_indexs):
+                if val == "N/A":
+                    print("N/A", end=f"{';' if i < len(list_indexs) - 1 else '\n'}")
+                else:
+                    print(row[val], end=f"{';' if i < len(list_indexs) - 1 else '\n'}")
+
+
+print_columns("library_books.csv", ["Автор", "Рейтинг"])
